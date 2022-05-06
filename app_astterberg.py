7b42335342664507f22f816e0132a79855ef7b91
ErikaVeleta <104532904+ErikaVeleta@users.noreply.github.com>
2022-05-06 13:11:55 -0600

    Update app_astterberg.py

--- app_astterberg.py
+++ app_astterberg.py
@@ -1,15 +1,41 @@
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
-st.slidebar.title ("Software para el estudio del índice de plasticidad del suelo (IP)")
+st.slidebar.title ("Software para el estudio de la composición del suelo fino")
 INTRODUCCIÓN
 
-st.image ("https://concepto.de/wp-content/uploads/2021/11/tipos-de-suelos-e1637359333414.jpg")
-
-capsula=st.number_input("Escribe el peso de cápsula (gr)",0)
-st.write(capsula)
-humedo=st.number_input("Escribe el peso de cápsula + suelo húmedo (gr)",0)
-st.write(humedo)
-seco=st.number_input("Escribe el peso de cápsula + suelo seco (gr)",0)
-st.write(seca)
-LL=
+st.image("https://concepto.de/wp-content/uploads/2021/11/tipos-de-suelos-e1637359333414.jpg")
+LL=st.number_input("Escribe el Límite Líquido (%)",0)
+st.write(LL)
+plastico=st.number_input("Escribe el Límite Plástico (%)",0)
+st.write(LP)
+w=st.number_input("Escribe la Humedad Natural del suelo (%)",0)
+st.write(w)
+IP=LL-LP
+IP=round(IP,3)
+IL=(w-LP)/LP
+IL=round(IL,3)
+IC=(LL-w)/IP
+IC=round(IL,3)
+cal=st.button ("Calcular")
+sr.write(cal)
+if cal<1:
+  st.write("# El Índice de Plasticidad (IP) es:"IP,"%")
+  st.write("El material corresponde a los suelos friables o desmenuzables")
+  st.write("# El Índice Líquido(IL) es:"IL,"%")
+  st.write("# El Índice Consistencia(IC) es:"IC,"%")
+else if cal>1&&cal<7:
+  st.write("# El IP es:"IP,"%")
+  st.write("El material corresponde a los suelos débilmente plásticos")
+  st.write("# El Índice Líquido(IL) es:"IL,"%")
+  st.write("# El Índice Consistencia(IC) es:"IC,"%")
+ else if cal>7&&cal<15:
+  st.write("# El IP es:"IP,"%")
+  st.write("El material corresponde a los suelos medianamente plásticos")
+  st.write("# El Índice Líquido(IL) es:"IL,"%")
+  st.write("# El Índice Consistencia(IC) es:"IC,"%")
+else if cal>15:
+  st.write("# El IP es:"IP,"%")
+  st.write("El material corresponde a los suelos altamente plásticos")
+  st.write("# El Índice Líquido(IL) es:"IL,"%")
+  st.write("# El Índice Consistencia(IC) es:"IC,"%")
+  
+  
