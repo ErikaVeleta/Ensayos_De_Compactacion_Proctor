3cced6ff5ddc5297dbc24f6f0e5b1bf92ed36ed4
ErikaVeleta <104532904+ErikaVeleta@users.noreply.github.com>
2022-05-06 14:13:58 -0600

    Update app_astterberg.py

--- app_astterberg.py
+++ app_astterberg.py
@@ -11,8 +11,6 @@ w=st.number_input("Escribe la Humedad Natural del suelo (%)")
 st.write(w)
 IP=LL-LP
 IP=round(IP,3)
-IL=(w-LP)/LP
-IC=(LL-w)/IP
 cal=st.button ("Calcular")
 st.write(cal)
 if cal<1:
@@ -25,15 +23,21 @@ if cal<1:
 elif cal in range(1,7):
   st.write("# El Índice de Plasticidad(IP) es:",IP,"%")
   st.write("El material corresponde a los suelos débilmente plásticos")
+  IL=(w-LP)/LP
   st.write("# El Índice Líquido(IL) es:",IL,"%")
+  IC=(LL-w)/IP
   st.write("# El Índice Consistencia(IC) es:",IC,"%")
 elif cal in range (7,15):
   st.write("# El Índice de Plasticidad(IP) es:",IP,"%")
   st.write("El material corresponde a los suelos medianamente plásticos")
+  IL=(w-LP)/LP
   st.write("# El Índice Líquido(IL) es:",IL,"%")
+  IC=(LL-w)/IP
   st.write("# El Índice Consistencia(IC) es:",IC,"%")
 else:
   st.write("# El Índice de Plasticidad(IP) es:",IP,"%")
   st.write("El material corresponde a los suelos altamente plásticos")
+  IL=(w-LP)/LP
   st.write("# El Índice Líquido(IL) es:",IL,"%")
+  IC=(LL-w)/IP
   st.write("# El Índice Consistencia(IC) es:",IC,"%")
