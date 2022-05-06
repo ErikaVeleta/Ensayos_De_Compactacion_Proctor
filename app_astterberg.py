fcf31e71b2d38082a8911b9b4ee163f9ed5744e5
ErikaVeleta <104532904+ErikaVeleta@users.noreply.github.com>
2022-05-06 17:23:31 -0600

    Update app_astterberg.py

--- app_astterberg.py
+++ app_astterberg.py
@@ -17,12 +17,12 @@ if cal<1:
   st.write("# El Índice de Plasticidad(IP) es:",IP,"%")
   st.write("El material corresponde a los suelos friables o desmenuzables")
   st.header("""
-  CARACTERÍSTICAS:
-  1.Son suelos limosos.
-  2.Tienen gránulos de tamaño intermedio.
-  2.Son fértiles y Fáciles de trabajar.
-  3.Suave y sedoso al tacto.
-  4.Forman terrones fáciles de desagregar cuando están secos.""")
+  CARACTERÍSTICAS:""")
+  st.write("1.Son suelos limosos.")
+  st.write("2.Tienen gránulos de tamaño intermedio.")
+  st.write("3.Son fértiles y Fáciles de trabajar.")
+  st.write("4.Suave y sedoso al tacto.")
+  st.write("5.Forman terrones fáciles de desagregar cuando están secos.")
   IL=(w-LP)/LP
   st.write("# El Índice Líquido(IL) es:",IL,"%")
   IC=(LL-w)/IP
@@ -32,12 +32,12 @@ elif cal in range(1,7):
   st.write("# El Índice de Plasticidad(IP) es:",IP,"%")
   st.write("El material corresponde a los suelos débilmente plásticos")
   st.header("""
-  CARACTERÍSTICAS:
-  1.Son suelos arcillosos.
-  2.Predomina el mineral caolinita.
-  3.Baja permeabilidad.
-  4.Los suelos con caolinita como mineral de arcilla presentan un comportamiento normal en los ensayos.
-  5.El efecto del aumento de humedad sobre las propiedades del suelo generalmente, no es importante.""")
+  CARACTERÍSTICAS:""")
+  st.write("1.Son suelos arcillosos.")
+  st.write("2.Predomina el mineral caolinita.")
+  st.write("3.Baja permeabilidad.")
+  st.write("4.Los suelos con caolinita como mineral de arcilla presentan un comportamiento normal en los ensayos.")
+  st.write("5.El efecto del aumento de humedad sobre las propiedades del suelo generalmente, no es importante.")
   IL=(w-LP)/LP
   st.write("# El Índice Líquido(IL) es:",IL,"%")
   IC=(LL-w)/IP
@@ -47,11 +47,11 @@ elif cal in range (7,15):
   st.write("# El Índice de Plasticidad(IP) es:",IP,"%")
   st.write("El material corresponde a los suelos medianamente plásticos")
   st.header("""
-  CARACTERÍSTICAS:
-  1.Son suelos arcillosos.
-  2.Constituida por los minerales illita y mica.
-  3.Son dioctaédricas (presencia en la capa octaédrica de cationes trivalentes).
-  4.Posibilidad de retener y liberar iones que pueden ser utilizados por las plantas para su alimento.""")
+  CARACTERÍSTICAS:""")
+  st.write("1.Son suelos arcillosos.")
+  st.write("2.Constituida por los minerales illita y mica.")
+  st.write("3.Son dioctaédricas (presencia en la capa octaédrica de cationes trivalentes).")
+  st.write("4.Posibilidad de retener y liberar iones que pueden ser utilizados por las plantas para su alimento.")
   IL=(w-LP)/LP
   st.write("# El Índice Líquido(IL) es:",IL,"%")
   IC=(LL-w)/IP
@@ -61,12 +61,12 @@ else:
   st.write("# El Índice de Plasticidad(IP) es:",IP,"%")
   st.write("El material corresponde a los suelos altamente plásticos")
   st.header("""
-  CARACTERÍSTICAS:
-  1.Son suelos arcillosos.
-  2.Materiales con contenidos apreciables de Montmorillonita.
-  3.Baja permeabilidad. 
-  4.El efecto del aumento de humedad puede resultar en una disminución importante de la resistencia al cortante. 
-  5.La Montmorillonita tiene un alto nivel de reacción con el cemento y la cal.""")
+  CARACTERÍSTICAS:""")
+  st.write("1.Son suelos arcillosos.")
+  st.write("2.Materiales con contenidos apreciables de Montmorillonita.")
+  st.write("3.Baja permeabilidad.")
+  st.write("4.El efecto del aumento de humedad puede resultar en una disminución importante de la resistencia al cortante.")
+  st.write("5.La Montmorillonita tiene un alto nivel de reacción con el cemento y la cal.")
   IL=(w-LP)/LP
   st.write("# El Índice Líquido(IL) es:",IL,"%")
   IC=(LL-w)/IP
